99d8fe56c0747dedccd5a5419d77b268681e76c1
Taylor T. Wood <ttwood@okapi.uvm.edu>
2013-11-08 00:56:12 -0500

    First version home

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Up in Arms Brewing Company</title>
+<meta charset="utf-8">
+<meta name="author" content="Taylor Wood">
+<meta name="description" content="Up in Arms Brewery">
+    
+<link rel="stylesheet"
+	  href="style.css"
+      media="screen">
+
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+    
+</head>
+
+<body id="Sample">
+<!-- ###########   Navigation menu     ################## -->
+
+<?php 
+ include ("nav.php");
+?>
+
+
+<!-- ###########   Body   ################## -->
+
+<section id="main">
+
+<header>Up in Arms Brewery</header>
+
+<p>
+
+Welcome to our website in progress. We love customer suggestions, so if there is any content you'd like to see here, let us know!
+
+Up in Arms Brewery has the goal of providing a fresh, constantly-changing selection of beers to quench the demand of the local Vermont 
+beer-drinking community. We love what we do, and hope that it comes across as you are enjoying one of our unique brews. Our mission is 
+to source locally, obtaining hops from our brewery's hop yards, malt from local farmers, and the clearest of natural spring water 
+that we find rushing from the peaks of our beautiful VT mountains. 
+
+More to come,
+
+-Taylor and Jim
+    
+</p>
+
+</section>
+<!-- ###########  search for  html5 article vs section  ################## -->
+
+<?php
+ include ("footer.php")
+?>
+
+</body>
+</html>
